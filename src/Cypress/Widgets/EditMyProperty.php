f5d1219cfcfc5effaaf98ca8911ba27d9da1150c
Ismail Zare <zarehesmaiel@gmail.com>
2022-04-12 22:02:12 +0430

    Added profile error

--- src/Cypress/Widgets/EditMyProperty.php
+++ src/Cypress/Widgets/EditMyProperty.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace Armincms\Iranmobleh\Cypress\Widgets;
-
-use Armincms\Contract\Gutenberg\Templates\Pagination; 
+ 
 use Armincms\Contract\Gutenberg\Widgets\BootstrapsTemplate; 
 use Armincms\Contract\Gutenberg\Widgets\ResolvesDisplay;  
 use Armincms\Iranmobleh\Cypress\Fragments\PropertyForm;   
@@ -19,11 +18,13 @@ use Armincms\Koomeh\Nova\RoomType;
 use Armincms\Location\Nova\State;   
 use Armincms\Location\Nova\City;   
 use Armincms\Location\Nova\Zone;   
+use Laravel\Nova\Fields\BooleanGroup;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Select;    
 use Zareismail\Cypress\Http\Requests\CypressRequest;
 use Zareismail\Gutenberg\Gutenberg; 
 use Zareismail\Gutenberg\GutenbergWidget; 
+use Zareismail\Gutenberg\Templates\Blank; 
 
 class EditMyProperty extends GutenbergWidget
 {     
@@ -54,6 +55,19 @@ class EditMyProperty extends GutenbergWidget
 
         parent::boot($request, $layout); 
 
+        if (! $this->profileFilled($request)) { 
+
+            $template = $this->bootstrapTemplate(
+                $request, 
+                $layout, 
+                $this->metaValue('profile.error_view')
+            );
+
+            $this->displayUsing(function($attributes) use ($template) { 
+                return $template->gutenbergTemplate($attributes)->render(); 
+            });
+        }
+
         $this->withMeta([
             'errors' => $this->validationErrors($request),
             'resource' => $request->isFragmentRequest()
@@ -61,6 +75,43 @@ class EditMyProperty extends GutenbergWidget
                 : null,
         ]);
     }  
+
+    public function profileFilled($request) {
+        if ($this->metaValue('profile.guarded.avatar') &&
+            is_null($request->user()->media)
+            ) {
+            return false;
+        }
+
+        return collect($request->user()->profile)->filter(function($value, $key) {
+            return $this->metaValue("profile.guarded.{$key}") && blank($value);
+        })->isEmpty();
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public static function fields($request)
+    {  
+        return [ 
+            Select::make(__('Display Profile Error By'), 'config->profile->error_view')
+                ->options(Gutenberg::cachedTemplates()->forHandler(Blank::class)->keyBy->getKey()->map->name)  
+                ->displayUsingLabels()
+                ->required()
+                ->rules('required'), 
+
+            BooleanGroup::make(__('Required profile'), 'config->profile->guarded')->options([
+                'firstname' => __('First Name'),
+                'lastname' => __('Last Name'),
+                'mobile' => __('Mobile Number'),
+                'phone' => __('Phone Number'),
+                'avatar' => __('Avatar'),
+            ]),
+        ];
+    }
 
     /**
      * Serialize the widget fro template.
